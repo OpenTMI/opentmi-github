fb8850d0387b34d74fa89073a933cb8e1f75b107
Jussi Vatjus-Anttila <jussiva@gmail.com>
2015-10-12 21:19:07 +0100

    just cleaning a little

--- index.js
+++ index.js
@@ -4,58 +4,55 @@ var winston = require('winston');
 var _ = require('underscore');
 
 function AddonGithub (app, server, io, passport){
-  var self = this;
-	this.name = 'GitHub addon';
-	this.description = 'Integrate Github to TMT';
-  this.listDependencies = ['github'];
+    var self = this;
+    this.name = 'GitHub addon';
+    this.description = 'Integrate Github to TMT';
+    this.listDependencies = ['github'];
   
-  var github;
-	this.register = function(){
+    var github;
     var cfg = nconf.get('github');
-    if( !cfg || !cfg.authentication ) {
-      winston.error('github not configured');
-      return;
-    }
-
-		app.get('/github', function(req, res){
-		  res.json({ok: 1});
-		});
-
-    github = self.github = new GitHubApi({
-        // required 
-        version: "3.0.0",
-        // optional 
-        //debug: true,  
-        timeout: 5000,
-    });
-    github.authenticate(
-        nconf.get('github').authentication
-    );
-/*
-    github.repos.getAll({}, function(error, repos){
-      console.log("repos.getAll()");
-      console.log(error);
-      console.log(repos);
-    });*/
-    var lookupOrg = function() {
-      github.orgs.get( {org: cfg.orgId }, function(err, res){
-        if( err ){
-          return winston.error(err);
-        }
-        global.pubsub.emit('github', {
-          public_repos: res.public_repos,
-          private_repos: res.total_private_repos,
-          disk_usage: res.disk_usage
-        })
-      });
-    }
-
-    setInterval( lookupOrg, 10000 );
     
-	}
+    this.register = function(){
+        
+        if( !cfg || !cfg.authentication ) {
+          winston.error('github not configured');
+          return;
+        }
 
+        app.get('/github', function(req, res){
+          res.json({ok: 1});
+        });
 
-  return this;
+        github = self.github = new GitHubApi({
+            // required 
+            version: "3.0.0",
+            // optional 
+            //debug: true,  
+            timeout: 5000,
+        });
+        github.authenticate( cfg.authentication );
+        /*
+        github.repos.getAll({}, function(error, repos){
+          console.log("repos.getAll()");
+          console.log(error);
+          console.log(repos);
+        });*/
+        var lookupOrg = function() {
+          github.orgs.get( {org: cfg.orgId }, function(err, res){
+            if( err ){
+              return winston.error(err);
+            }
+            global.pubsub.emit('github', {
+              public_repos: res.public_repos,
+              private_repos: res.total_private_repos,
+              disk_usage: res.disk_usage
+            })
+          });
+        }
+        setInterval( lookupOrg, 10000 );
+    
+    }
+    return this;
 }
 
 exports = module.exports = AddonGithub;
